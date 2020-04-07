7660019df8bfbc46240d5639bcc833be9dcee65b
guivalerioS <guigui-valerio@hotmail.com>
2020-04-06 22:32:11 -0300

    terms and conditions

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,18 @@
 import React, { useRef, useState } from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Modal from 'react-native-modal';
 import BackgroundInitial from '~/components/BackgroundInitial';
 import { signUpRequest } from '~/store/modules/auth/actions';
+
+// import TermsAndConditions from '~/components/TermsandConditions';
 
 import {
   Container,
@@ -79,15 +89,206 @@ export default function SignUp({ navigation }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isModalVisible, setisModalVisible] = useState(false);
+  const [accepted, setAccepted] = useState(false);
+  const [confirmed, setConfirmed] = useState(false);
 
   const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit() {
-    dispatch(signUpRequest(name, email, password, navigation));
+    // dispatch(signUpRequest(name, email, password, navigation));
+    setisModalVisible(!isModalVisible);
   }
+
+  function handleLogin() {
+    setisModalVisible(!isModalVisible);
+    dispatch(signUpRequest(name, email, password, navigation));
+    // navigation.navigate('SignIn');
+  }
+
+  function handleConfirm() {
+    alert("Termos e Condições Aceitados");
+    setConfirmed(true);
+  }
+
+  
+
+  const isCloseToBottom = ({
+    layoutMeasurement,
+    contentOffset,
+    contentSize,
+  }) => {
+    const paddingToBottom = 20;
+    return (
+      layoutMeasurement.height + contentOffset.y >=
+      contentSize.height - paddingToBottom
+    );
+  };
+
+  const { width, height } = Dimensions.get('window');
+
+  const styles = {
+    container: {
+      marginTop: 20,
+      marginLeft: 10,
+      marginRight: 10,
+      backgroundColor: '#FFF',
+      borderRadius: 15,
+    },
+    title: {
+      fontSize: 22,
+      alignSelf: 'center',
+    },
+    tcP: {
+      marginTop: 10,
+      marginBottom: 10,
+      fontSize: 12,
+    },
+    tcL: {
+      marginLeft: 10,
+      marginTop: 10,
+      marginBottom: 10,
+      fontSize: 12,
+    },
+    tcContainer: {
+      marginTop: 15,
+      marginBottom: 15,
+      height: height * 0.7,
+    },
+
+    button: {
+      backgroundColor: '#136AC7',
+      borderRadius: 5,
+      padding: 10,
+    },
+
+    buttonDisabled: {
+      backgroundColor: '#999',
+      borderRadius: 5,
+      padding: 10,
+    },
+
+    buttonC: {
+      backgroundColor: '#4EAD93',
+      borderRadius: 5,
+      padding: 10,
+    },
+
+    buttonLabel: {
+      fontSize: 14,
+      color: '#FFF',
+      alignSelf: 'center',
+    },
+  };
 
   return (
     <BackgroundInitial>
+      <Modal isVisible={isModalVisible}>
+        <View style={{ flex: 1 }}>
+          <View style={styles.container}>
+            <Text style={styles.title}>Termos e Condições</Text>
+            <ScrollView
+              style={styles.tcContainer}
+              onScroll={({ nativeEvent }) => {
+                if (isCloseToBottom(nativeEvent)) {
+                  setAccepted(true);
+                }
+              }}
+            >
+              <Text style={styles.tcP}>
+                Welcome to our website. If you continue to browse and use this
+                website, you are agreeing to comply with and be bound by the
+                following terms and conditions of use, which together with our
+                privacy policy govern [business name]’s relationship with you in
+                relation to this website. If you disagree with any part of these
+                terms and conditions, please do not use our website.
+              </Text>
+              <Text style={styles.tcP}>
+                The term ‘[business name]’ or ‘us’ or ‘we’ refers to the owner
+                of the website whose registered office is [address]. Our company
+                registration number is [company registration number and place of
+                registration]. The term ‘you’ refers to the user or viewer of
+                our website.
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} The content of the pages of this website is for your
+                general information and use only. It is subject to change
+                without notice.
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} This website uses cookies to monitor browsing
+                preferences. If you do allow cookies to be used, the following
+                personal information may be stored by us for use by third
+                parties: [insert list of information].
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} Neither we nor any third parties provide any warranty
+                or guarantee as to the accuracy, timeliness, performance,
+                completeness or suitability of the information and materials
+                found or offered on this website for any particular purpose. You
+                acknowledge that such information and materials may contain
+                inaccuracies or errors and we expressly exclude liability for
+                any such inaccuracies or errors to the fullest extent permitted
+                by law.
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} Your use of any information or materials on this
+                website is entirely at your own risk, for which we shall not be
+                liable. It shall be your own responsibility to ensure that any
+                products, services or information available through this website
+                meet your specific requirements.
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} This website contains material which is owned by or
+                licensed to us. This material includes, but is not limited to,
+                the design, layout, look, appearance and graphics. Reproduction
+                is prohibited other than in accordance with the copyright
+                notice, which forms part of these terms and conditions.
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} All trademarks reproduced in this website, which are
+                not the property of, or licensed to the operator, are
+                acknowledged on the website. Unauthorised use of this website
+                may give rise to a claim for damages and/or be a criminal
+                offence.
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} From time to time, this website may also include
+                links to other websites. These links are provided for your
+                convenience to provide further information. They do not signify
+                that we endorse the website(s). We have no responsibility for
+                the content of the linked website(s).
+              </Text>
+              <Text style={styles.tcL}>
+                {'\u2022'} Your use of this website and any dispute arising out
+                of such use of the website is subject to the laws of England,
+                Northern Ireland, Scotland and Wales.
+              </Text>
+              <Text style={styles.tcP}>
+                The use of this website is subject to the following terms of use
+              </Text>
+            </ScrollView>
+
+            <TouchableOpacity
+              disabled={!accepted}
+              onPress={handleConfirm}
+              style={accepted ? styles.button : styles.buttonDisabled}
+            >
+              <Text style={styles.buttonLabel}>Concordo</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              disabled={!confirmed}
+              onPress={handleLogin}
+              style={confirmed ? styles.buttonC : styles.buttonDisabled}
+            >
+              <Text style={styles.buttonLabel}>Finalizar Cadastro</Text>
+            </TouchableOpacity>
+          </View>
+          {/* <Button title="Ir para Login" onPress={handleLogin} /> */}
+        </View>
+      </Modal>
+
       <Container>
         <Cima xml={xmlCima} />
 
