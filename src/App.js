cd1d4000c8536d1fb4270f1b86004e1f38d3c9d6
Jorge Rossi <jorgefzrossi@gmail.com>
2020-06-08 15:53:53 -0300

    Configure firebase auth and save fcm token on database

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import auth from '@react-native-firebase/auth';
+import messaging from '@react-native-firebase/messaging';
+import database from '@react-native-firebase/database';
 
 import createRouter from './routes';
 
@@ -9,6 +12,31 @@ export default function App() {
   const active = useSelector(state => state.auth.active);
   const profileId = useSelector(state => state.user.profile);
   const Routes = createRouter(signed, token, profileId, active);
+
+  async function saveTokenToDatabase(fcmToken) {
+    const userId = auth().currentUser.uid;
+
+    await database()
+      .ref(`users/${userId}`)
+      .update({ token: fcmToken });
+  }
+
+  useEffect(() => {
+    async function onAuthStateChanged(user) {
+      if (!user) return;
+
+      const fcmToken = await messaging().getToken();
+
+      saveTokenToDatabase(fcmToken);
+
+      messaging().onTokenRefresh(newToken => {
+        saveTokenToDatabase(newToken);
+      });
+    }
+
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
 
   return <Routes />;
 }
