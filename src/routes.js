852bf59e375449d12680f6bc42c2178808ae7f89
Guilherme valerio <guilherme.v@cristofoli.local>
2020-09-23 11:59:14 -0300

    drawer

--- src/routes.js
+++ src/routes.js
@@ -6,17 +6,17 @@ import { Image, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MIcon from 'react-native-vector-icons/MaterialIcons';
 import Notific from './assets/notific-branco.svg';
-import { DrawerActions } from 'react-navigation-drawer';
 
 import {
   createAppContainer,
   createBottomTabNavigator,
   createMaterialTopTabNavigator,
-  createDrawerNavigator,
   createStackNavigator,
   createSwitchNavigator,
   NavigationActions,
 } from 'react-navigation';
+import { DrawerActions, createDrawerNavigator } from 'react-navigation-drawer';
+
 import Logo from '~/assets/dashboard-06.svg';
 import MiddleButton from '~/assets/middlebutton.svg';
 
@@ -89,213 +89,11 @@ export default (isSigned = false, token, profileid, active) =>
 
         NewApp: createStackNavigator(
           {
-            NewAppBottom: createBottomTabNavigator(
-              {
-                Agendamentos: {
-                  screen: Dashboard,
-                  navigationOptions: {
-                    tabBarOptions: {
-                      activeTintColor: colors.primary,
-                      style: {
-                        backgroundColor: '#201D2E',
-                      },
-                    },
-                  },
-                },
-                Solicitations: {
-                  // navigationOptions: {
-                  //   tabBarOptions: {
-                  //     labelStyle: {
-                  //       fontSize: 10,
-                  //     },
-                  //     activeTintColor: '#FFFFFF',
-                  //     inactiveTintColor: '#15162c',
-                  //     indicatorStyle: {
-                  //       height: '100%',
-                  //       backgroundColor: '#15162c',
-                  //     },
-                  //     style: {
-                  //       backgroundColor: '#E5E5E5',
-                  //     },
-                  //   },
-                  // },
-                  screen: createSwitchNavigator({
-                    enterRoom,
-                    Chat,
-                  }),
-                  navigationOptions: {
-                    tabBarLabel: 'Chats',
-                    // tabBarOptions: {
-                    //   activeTintColor: '#4BB196',
-                    // },
-
-                    tabBarOptions: {
-                      activeTintColor: colors.primary,
-                      style: {
-                        backgroundColor: '#201D2E',
-                      },
-                    },
-                    tabBarIcon: ({ focused }) => {
-                      if (focused) {
-                        return (
-                          <Image
-                            source={solicitationIcon}
-                            style={{
-                              width: 24,
-                              height: 24,
-                              tintColor: '#4BB196',
-                            }}
-                          />
-                        );
-                      } else {
-                        return (
-                          <Image
-                            source={solicitationIcon}
-                            style={{
-                              width: 24,
-                              height: 24,
-                              tintColor: '#808080',
-                            }}
-                          />
-                        );
-                      }
-                    },
-                  },
-                },
-                Dashboard: {
-                  screen: Dashboard,
-                  navigationOptions: {
-                    tabBarOptions: {
-                      activeTintColor: colors.primary,
-                      style: {
-                        backgroundColor: '#201D2E',
-                      },
-                    },
-                    tabBarLabel: ' ',
-                    tabBarIcon: ({ tintColor }) => (
-                      <View
-                        style={{
-                          position: 'absolute',
-                          bottom: 3, // space from bottombar
-                          height: 39,
-                          width: 39,
-                          borderRadius: 39,
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                        }}
-                      >
-                        <MiddleButton
-                          style={{
-                            width: 39,
-                            height: 39,
-                            tintColor,
-                            alignContent: 'center',
-                          }}
-                        />
-                      </View>
-                    ),
-                  },
-                },
-                Estatisticas: {
-                  screen: Statistics,
-                  navigationOptions: {
-                    tabBarOptions: {
-                      activeTintColor: colors.primary,
-                      style: {
-                        backgroundColor: '#201D2E',
-                      },
-                    },
-                  },
-                },
-                Profile: {
-                  navigationOptions: {
-                    tabBarOptions: {
-                      activeTintColor: colors.primary,
-                      style: {
-                        backgroundColor: '#201D2E',
-                      },
-                    },
-                  },
-                  screen: Profile,
-                },
-              },
-              {
-                initialRouteName: 'Dashboard',
-              }
-            ),
             App: createDrawerNavigator(
               {
-                Painel: createMaterialTopTabNavigator({
-                  Agendamentos: {
-                    screen: Dashboard,
-                  },
-                  Solicitations: {
-                    navigationOptions: {
-                      tabBarOptions: {
-                        labelStyle: {
-                          fontSize: 10,
-                        },
-                        activeTintColor: '#FFFFFF',
-                        inactiveTintColor: '#15162c',
-                        indicatorStyle: {
-                          height: '100%',
-                          backgroundColor: '#15162c',
-                        },
-                        style: {
-                          backgroundColor: '#E5E5E5',
-                        },
-                      },
-                    },
-                    screen: createSwitchNavigator({
-                      enterRoom,
-                      Chat,
-                    }),
-                    navigationOptions: {
-                      tabBarLabel: 'Chats',
-                      // tabBarOptions: {
-                      //   activeTintColor: '#4BB196',
-                      // },
-
-                      tabBarOptions: {
-                        labelStyle: {
-                          fontSize: 10,
-                        },
-                        style: {
-                          backgroundColor: '#E5E5E5',
-                        },
-                      },
-                      tabBarIcon: ({ focused }) => {
-                        if (focused) {
-                          return (
-                            <Image
-                              source={solicitationIcon}
-                              style={{
-                                width: 24,
-                                height: 24,
-                                tintColor: '#4BB196',
-                              }}
-                            />
-                          );
-                        } else {
-                          return (
-                            <Image
-                              source={solicitationIcon}
-                              style={{
-                                width: 24,
-                                height: 24,
-                                tintColor: '#808080',
-                              }}
-                            />
-                          );
-                        }
-                      },
-                    },
-                  },
-
-                  Estatisticas: {
-                    screen: Statistics,
-                  },
-                }),
+                Painel: {
+                  screen: Dashboard,
+                },
                 Perfil: createMaterialTopTabNavigator({
                   Profile: {
                     navigationOptions: {
@@ -498,9 +296,144 @@ export default (isSigned = false, token, profileid, active) =>
                 contentComponent: ContentMenu,
               }
             ),
+            NewAppBottom: createBottomTabNavigator(
+              {
+                Agendamentos: {
+                  screen: Dashboard,
+                  navigationOptions: {
+                    tabBarOptions: {
+                      activeTintColor: colors.primary,
+                      style: {
+                        backgroundColor: '#201D2E',
+                      },
+                    },
+                  },
+                },
+                Solicitations: {
+                  // navigationOptions: {
+                  //   tabBarOptions: {
+                  //     labelStyle: {
+                  //       fontSize: 10,
+                  //     },
+                  //     activeTintColor: '#FFFFFF',
+                  //     inactiveTintColor: '#15162c',
+                  //     indicatorStyle: {
+                  //       height: '100%',
+                  //       backgroundColor: '#15162c',
+                  //     },
+                  //     style: {
+                  //       backgroundColor: '#E5E5E5',
+                  //     },
+                  //   },
+                  // },
+                  screen: createSwitchNavigator({
+                    enterRoom,
+                    Chat,
+                  }),
+                  navigationOptions: {
+                    tabBarLabel: 'Chats',
+                    // tabBarOptions: {
+                    //   activeTintColor: '#4BB196',
+                    // },
+
+                    tabBarOptions: {
+                      activeTintColor: colors.primary,
+                      style: {
+                        backgroundColor: '#201D2E',
+                      },
+                    },
+                    tabBarIcon: ({ focused }) => {
+                      if (focused) {
+                        return (
+                          <Image
+                            source={solicitationIcon}
+                            style={{
+                              width: 24,
+                              height: 24,
+                              tintColor: '#4BB196',
+                            }}
+                          />
+                        );
+                      } else {
+                        return (
+                          <Image
+                            source={solicitationIcon}
+                            style={{
+                              width: 24,
+                              height: 24,
+                              tintColor: '#808080',
+                            }}
+                          />
+                        );
+                      }
+                    },
+                  },
+                },
+                Dashboard: {
+                  screen: Dashboard,
+                  navigationOptions: {
+                    tabBarOptions: {
+                      activeTintColor: colors.primary,
+                      style: {
+                        backgroundColor: '#201D2E',
+                      },
+                    },
+                    tabBarLabel: ' ',
+                    tabBarIcon: ({ tintColor }) => (
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 3, // space from bottombar
+                          height: 39,
+                          width: 39,
+                          borderRadius: 39,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}
+                      >
+                        <MiddleButton
+                          style={{
+                            width: 39,
+                            height: 39,
+                            tintColor,
+                            alignContent: 'center',
+                          }}
+                        />
+                      </View>
+                    ),
+                  },
+                },
+                Estatisticas: {
+                  screen: Statistics,
+                  navigationOptions: {
+                    tabBarOptions: {
+                      activeTintColor: colors.primary,
+                      style: {
+                        backgroundColor: '#201D2E',
+                      },
+                    },
+                  },
+                },
+                Profile: {
+                  navigationOptions: {
+                    tabBarOptions: {
+                      activeTintColor: colors.primary,
+                      style: {
+                        backgroundColor: '#201D2E',
+                      },
+                    },
+                  },
+                  screen: Profile,
+                },
+              },
+              {
+                initialRouteName: 'Dashboard',
+              }
+            ),
           },
           {
             defaultNavigationOptions: ({ navigation }) => {
+              //  navigation.navigate('NewAppBottom');
               const pathAndParams =
                 navigation.router.getPathAndParamsForState(navigation.state) ||
                 {};
@@ -520,8 +453,9 @@ export default (isSigned = false, token, profileid, active) =>
                 },
                 headerLeft: (
                   <Touchable
-                    onPress={() =>
-                      navigation.dispatch(DrawerActions.toggleDrawer())
+                    onPress={
+                      () => navigation.dispatch(DrawerActions.toggleDrawer())
+                      //  navigation.navigate('NewAppBottom')
                     }
                   >
                     <Icon
